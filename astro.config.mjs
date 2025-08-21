58aed8cf67f4cec457baddd0768184576c3dd7e4
Pablo León Toro <pablo@redte.ch>
2025-08-20 20:08:51 -0400

    solucionando deploy 002

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,16 @@ import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
-adapter: netlify(),
+  adapter: netlify(),
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': new URL('./src/assets', import.meta.url).pathname,
+        '@components': new URL('./src/components', import.meta.url).pathname,
+        '@css': new URL('./src/css', import.meta.url).pathname,
+        '@data': new URL('./src/data', import.meta.url).pathname,
+        '@layouts': new URL('./src/layouts', import.meta.url).pathname,
+      },
+    },
+  },
 });
