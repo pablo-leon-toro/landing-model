0ca3f107aea451d31057a100d923435cec45b265
Pablo León Toro <pablo@redte.ch>
2025-08-22 00:08:20 -0400

    workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-  vite: {
-    resolve: {
-      alias: {
-        '@assets': new URL('./src/assets', import.meta.url).pathname,
-        '@components': new URL('./src/components', import.meta.url).pathname,
-        '@css': new URL('./src/css', import.meta.url).pathname,
-        '@data': new URL('./src/data', import.meta.url).pathname,
-        '@layouts': new URL('./src/layouts', import.meta.url).pathname,
-      },
-    },
-  },
+  site: 'https://pablo-leon-toro.github.io',
+  base: 'landing-model',
 });
